ad0d10564f14bf279f5a18c423e16eff4cfc8fba
blkurd <blkurd@hotmail.com>
2023-03-05 16:04:56 -0500

    Adding to the app

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
+finches = [
+
+      {'name': 'Wowo', 'breed': 'bird', 'description': 'furry', 'age': 2},
+      {'name': 'yeye', 'breed': 'skinny', 'description': 'fur', 'age': 5},
+
+]
+
 # Create your views here.
+# Define the home view
+def home(request):
+  # Include an .html file extension - unlike when rendering EJS templates
+  return render(request, 'home.html')
+
+def about(request):
+  # Include an .html file extension - unlike when rendering EJS templates
+  return render(request, 'about.html')
+
+def finches_index(request):
+  return render(request, 'finches/index.html', {'finches': finches })
