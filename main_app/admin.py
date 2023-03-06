86c3cf6c17f2da6144bdcfab5359c6353f26b223
blkurd <blkurd@hotmail.com>
2023-03-06 09:45:05 -0500

    Added Toy and Updated views

--- main_app/admin.py
+++ main_app/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 
 
 # Register your models here we want to use in our admin portal
-from .models import Finch
+from .models import Finch, Feeding, Toy
 
 admin.site.register(Finch)
+
+# register our new feeding model
+admin.site.register(Feeding)
+
+admin.site.register(Toy)
